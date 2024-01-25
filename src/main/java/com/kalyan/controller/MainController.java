cd9579272052fc3ca4081f9f71c71e5ba77bc370
thalarikalyan <thalarikalyaninfo@gmail.com>
2024-01-25 14:48:14 -0500

    Added Logger

--- src/main/java/com/kalyan/controller/MainController.java
+++ src/main/java/com/kalyan/controller/MainController.java
@@ -1,15 +1,18 @@
 package com.kalyan.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 
 import jakarta.annotation.PostConstruct;
 
 @Controller
 public class MainController {
+	public static Logger logger = LoggerFactory.getLogger(MainController.class);
 
 	@PostConstruct
 	public void m() {
-		System.out.println("MainController.m()");
+		logger.info("MainController.m()");
 
 	}
 
